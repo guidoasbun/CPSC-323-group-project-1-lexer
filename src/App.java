c5c7eae6de3b88d4fa83a7efe54efc65387331cd
Guido Asbun <guido@asbun.io>
2024-02-21 17:55:39 -0800

    input and output from file

--- src/App.java
+++ src/App.java
@@ -1,6 +1,22 @@
+import java.io.FileReader;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("CPSC 323 - Project 1");
-        System.out.println("Lexical Analyzer");
+        
+        String inputFileName = "inputFile/input_scode.txt";
+        String outputFileName = "outputFile/output_scode.txt";
+
+        Scanner fileInput = new Scanner(new FileReader(inputFileName));
+        PrintWriter fileOutput = new PrintWriter(outputFileName);
+
+        while (fileInput.hasNextLine()) {
+            String line = fileInput.nextLine();
+            fileOutput.println(line);
+        }
+        
+        fileInput.close();
+        fileOutput.close();
     }
 }
